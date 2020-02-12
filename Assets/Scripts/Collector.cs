212ff87de99ea1a4277aa3232d2a534497c9e5f8
SahanWickramage <sahanwickramage.16@cse.mrt.ac.lk>
2020-02-12 21:55:19 +0530

    fruit catching game finished

---
+++ Assets/Scripts/Collector.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Collector : MonoBehaviour {
+
+	void OnTriggerEnter2D(Collider2D target){
+		if (target.tag == "Bomb" || target.tag == "Fruit") {
+			Destroy (target.gameObject);
+		}
+	}
+}
---
+++ Assets/Scripts/ScoreController.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ScoreController : MonoBehaviour {
+	
+	public Text scoreText;
+
+	private int score;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		scoreText.text = score.ToString ();
+	}
+
+
+	void OnTriggerEnter2D(Collider2D target){
+
+		if (target.tag == "Bomb") {
+			SceneManager.LoadScene (SceneManager.GetActiveScene().buildIndex);
+		}
+		
+	}
+	void OnTriggerExit2D(Collider2D target){
+
+		if (target.tag == "Fruit") {
+			Destroy (target.gameObject);
+			score++;
+		}
+	}
+
+	/*IEnumerator WaitTilRestart(){
+
+		yield return new WaitForSeconds (1.5f);
+		SceneManager.LoadScene (SceneManager.GetActiveScene().buildIndex);
+	}*/
+}
