fe0bd98b7e0883ea54cc9b17fcbe2a2b0371a4d0
SahanWickramage <sahanwickramage.16@cse.mrt.ac.lk>
2020-02-12 14:01:56 +0530

    added fruit falling

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawner : MonoBehaviour {
+
+	public GameObject[] fruits;
+	public GameObject bomb;
+
+	public float xBounds, yBound;
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine (SpawnRandomGameObject ());
+	}
+	
+	IEnumerator SpawnRandomGameObject(){
+
+		yield return new WaitForSeconds (Random.Range (1, 2));
+
+		int randomFruit = Random.Range (0, 2);
+
+		if (Random.value <= .6f) {
+			Instantiate (fruits [randomFruit], new Vector2 (Random.Range (-xBounds, xBounds), yBound), Quaternion.identity);
+		} else {
+			Instantiate (bomb, new Vector2 (Random.Range (-xBounds, xBounds), yBound), Quaternion.identity);
+		}
+
+		StartCoroutine (SpawnRandomGameObject ());
+	}
+}
