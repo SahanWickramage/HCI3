cf6cb3a920edf361a68f98292fc5bfa7884c7547
SahanWickramage <sahanwickramage.16@cse.mrt.ac.lk>
2020-02-13 09:56:28 +0530

    kinect controls added

--- Assets/Scripts/HatMovement.cs
+++ Assets/Scripts/HatMovement.cs
@@ -1,29 +1,77 @@
 ﻿using UnityEngine;
 using System.Collections;
+using Windows.Kinect;
 
 public class HatMovement : MonoBehaviour {
 
+	public int hasKinect;
+
+	//for kinect controls
+	public GameObject BodySrcManager;
+	public JointType TrackedJoint;
+	private BodySourceManager bodyManager;
+	private Body[] bodies;
+	public float multiplier = 10f;
+
+	//for keyboard controls
 	private Rigidbody2D myBody;
 	public float speed, xBound;
 
 	// Use this for initialization
 	void Start () {
-		myBody = GetComponent<Rigidbody2D> ();
+		if (hasKinect == 0) {
+			myBody = GetComponent<Rigidbody2D> ();
+		} else if (hasKinect == 1) {
+			if (BodySrcManager == null) {
+				Debug.Log ("Assign body source manager.");
+			} else {
+				bodyManager = BodySrcManager.GetComponent<BodySourceManager> ();
+			}
+		}
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-		float h = Input.GetAxisRaw("Horizontal");
 
-		//print (h);
-		if (h > 0) {
-			myBody.velocity = Vector2.right * speed;
-		} else if (h < 0) {
-			myBody.velocity = Vector2.left * speed;
-		} else {
-			myBody.velocity = Vector2.zero;
+		//controlls when kinect presents
+		if (hasKinect == 0) {
+			//Debug.Log ("keyboard");
+			float h = Input.GetAxisRaw("Horizontal");
+
+			//print (h);
+			if (h > 0) {
+				myBody.velocity = Vector2.right * speed;
+			} else if (h < 0) {
+				myBody.velocity = Vector2.left * speed;
+			} else {
+				myBody.velocity = Vector2.zero;
+			}
+
+			transform.position = new Vector2 (Mathf.Clamp(transform.position.x, -xBound, xBound), transform.position.y);
 		}
-
-		transform.position = new Vector2 (Mathf.Clamp(transform.position.x, -xBound, xBound), transform.position.y);
+		//controlls when not nkinect presents
+			else if (hasKinect == 1){
+			//Debug.Log ("kinect");
+			if (bodyManager == null) {
+				Debug.Log ("bodyManager is null.");
+				return;
+			}
+			bodies = bodyManager.GetData ();
+			if (bodies == null) {
+				return;
+			}
+			Debug.Log ("have bodies");
+			foreach (var body in bodies) {
+				if (body == null){
+					continue;
+				}
+				if (body.IsTracked){
+					Debug.Log ("body is tracked");
+					var pos = body.Joints[TrackedJoint].Position;
+					//gameObject.transform.position = new Vector3(pos.X * multiplier, pos.Y * multiplier);
+					transform.position = new Vector2(Mathf.Clamp(pos.X * multiplier, -xBound, xBound), transform.position.y);
+				}
+			}
+		}
 	}
 }
